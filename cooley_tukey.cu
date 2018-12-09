d4e8768a04b2767b2ce61320a7ab1a3648c3c1c4
Kazuaki Matsumura <matsumura.k.ak@m.titech.ac.jp>
2018-12-09 20:40:07 +0800

    (Cooley Tukey) Grouping not working

--- cooley_tukey.cu
+++ cooley_tukey.cu
@@ -1,4 +1,5 @@
 #include "common.hu"
+#include <cooperative_groups.h>
 
 #define CUDART_PI_F 3.141592654f
 
@@ -22,142 +23,77 @@ __device__ __forceinline__ DATA_TYPE twiddle(DATA_TYPE a, size_t n, size_t block
     return mul(a, __floats2half2_rn(cosf(f), - sinf(f)));
 }
 
-__device__ void _fft(DATA_TYPE *data, DATA_TYPE *fm, DATA_TYPE *workspace, size_t n, size_t id, cublasHandle_t handle)
+__global__ void fft(DATA_TYPE *data)
 {
-    if (n > 4) {
-        _fft(data, fm, workspace, n / 4, id, handle);
-        __syncthreads();
-    }
+    cooperative_groups::grid_group g = cooperative_groups::this_grid();
 
-    size_t gblock = id / n;
-    size_t offset = gblock * n;
-    size_t block = gblock - (gblock / 4) * 4;
-    size_t pos   = id - offset;
-    size_t row   = pos / 4;
-    size_t col   = pos - row;
-
-    // transpose, multiply by twiddle factor (W^(block, row))
-    DATA_TYPE x = twiddle(data[offset + col * n + row], n, block, row);
-
-    // gemm
-    half *a_real = (half*)workspace + DATA_SIZE * 0;
-    half *a_imag = (half*)workspace + DATA_SIZE * 1;
-    half *c_real = (half*)workspace + DATA_SIZE * 2;
-    half *c_imag = (half*)workspace + DATA_SIZE * 3;
-
-    half *fm_real = (half*)fm;
-    half *fm_imag = (half*)fm + 16;
-
-    a_real[id] = __high2half(x);
-    a_imag[id] = __low2half(x);
-
-    /* real part */
-    half alpha = 1;
-    half beta = 0;
-
-    cublasCheckReturn(cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                                  DATA_SIZE / 4, 4, 4,
-                                  &alpha,
-                                  a_real, DATA_SIZE,
-                                  fm_real, 16,
-                                  &beta,
-                                  c_real, DATA_SIZE));
-
-    alpha = -1;
-    beta = 1;
-
-    cublasCheckReturn(cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                                  DATA_SIZE / 4, 4, 4,
-                                  &alpha,
-                                  a_imag, DATA_SIZE,
-                                  fm_imag, 16,
-                                  &beta,
-                                  c_real, DATA_SIZE));
-
-    /* imaginal part */
-    alpha = 1;
-    beta = 0;
-
-    cublasCheckReturn(cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                                  DATA_SIZE / 4, 4, 4,
-                                  &alpha,
-                                  a_real, DATA_SIZE,
-                                  fm_imag, 16,
-                                  &beta,
-                                  c_real, DATA_SIZE));
-
-    alpha = 1;
-    beta = 1;
-
-    cublasCheckReturn(cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                                  DATA_SIZE / 4, 4, 4,
-                                  &alpha,
-                                  a_imag, DATA_SIZE,
-                                  fm_real, 16,
-                                  &beta,
-                                  c_real, DATA_SIZE));
-
-    /* finish */
-    data[id] = __halves2half2(c_real[id], c_imag[id]);
-}
-
-__global__ void fft(DATA_TYPE *data, DATA_TYPE *fm, DATA_TYPE *workspace)
-{
+    __shared__ DATA_TYPE sm[1024];
     size_t id = blockIdx.x * blockDim.x + threadIdx.x;
 
-    cublasHandle_t handle;
-    cublasCheckReturn(cublasCreate(&handle));
-    //cublasCheckReturn(cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH));
+    const float f_real[] = { 
+        1.f,  1.f,  1.f,  1.f,
+        1.f,  0.f, -1.f,  0.f,
+        1.f, -1.f,  1.f, -1.f,
+        1.f,  0.f, -1.f,  0.f
+    };
 
-    _fft(data, fm, workspace, DATA_SIZE / 4, id, handle);
+    const float f_imag[] = {
+        0.f,  0.f, 0.f,  0.f,
+        0.f,  1.f, 0.f, -1.f,
+        0.f,  0.f, 0.f,  0.f,
+        0.f, -1.f, 0.f,  1.f
+    };
 
-    cublasCheckReturn(cublasDestroy(handle));
+    size_t idxl4 = threadIdx.x & 3 * 4;
+    DATA_TYPE f_0 = __floats2half2_rn(f_real[idxl4 + 0], f_imag[idxl4 + 0]);
+    DATA_TYPE f_1 = __floats2half2_rn(f_real[idxl4 + 1], f_imag[idxl4 + 1]);
+    DATA_TYPE f_2 = __floats2half2_rn(f_real[idxl4 + 2], f_imag[idxl4 + 2]);
+    DATA_TYPE f_3 = __floats2half2_rn(f_real[idxl4 + 3], f_imag[idxl4 + 3]);
+
+    for (size_t n  = 4; n <= DATA_SIZE / 4; n *= 4) {
+        if (n != 4) {
+            g.sync();
+        }
+
+        size_t gblock = id / n;
+        size_t offset = gblock * n;
+        size_t block = gblock - (gblock / 4) * 4;
+        size_t pos   = id - offset;
+        size_t row   = pos / 4;
+        size_t col   = pos - row * 4;
+
+        // transpose, multiply by twiddle factor (W^(block, row))
+        DATA_TYPE x = twiddle(data[offset + col * n + row], n, block, row);
+
+        sm[threadIdx.x] = x;
+        __syncwarp();
+
+        // gemm
+        data[id] = __hadd2(__hadd2(mul(sm[threadIdx.x - col + 0], f_0),
+                                   mul(sm[threadIdx.x - col + 1], f_1)),
+                           __hadd2(mul(sm[threadIdx.x - col + 2], f_2),
+                                   mul(sm[threadIdx.x - col + 3], f_3)));
+    }
 }
 
 std::vector<float> benchmark(DATA_TYPE *output,
                              DATA_TYPE *data,
                              cudaEvent_t start, cudaEvent_t stop)
 {
-    DATA_TYPE *dev_output, *dev_middle, *dev_data, *middle, *dev_workspace, *dev_fm, *fm;
+    DATA_TYPE *dev_output, *dev_middle, *dev_data, *middle;
     std::vector<float> time(2);
 
     /*
       Setup
     */
     cudaCheckReturn(cudaMallocHost(&middle, DATA_SIZE * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&fm, 4 * 4 * sizeof(DATA_TYPE)));
 
     cudaCheckReturn(cudaMalloc(&dev_data,      DATA_SIZE * sizeof(DATA_TYPE)));
     cudaCheckReturn(cudaMalloc(&dev_middle,    DATA_SIZE * sizeof(DATA_TYPE)));
     cudaCheckReturn(cudaMalloc(&dev_output,    DATA_SIZE * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMalloc(&dev_workspace, 2 * DATA_SIZE * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMalloc(&dev_fm,        DATA_SIZE * sizeof(DATA_TYPE)));
 
     cudaCheckReturn(cudaMemcpy(dev_middle, data, DATA_SIZE * sizeof(DATA_TYPE),
                                cudaMemcpyHostToDevice));
-
-    {
-        const float ff[] = { 
-            // real part
-            1.f,  1.f,  1.f,  1.f,
-            1.f,  0.f, -1.f,  0.f,
-            1.f, -1.f,  1.f, -1.f,
-            1.f,  0.f, -1.f,  0.f,
-
-            // imaginal part
-            0.f,  0.f, 0.f,  0.f,
-            0.f,  1.f, 0.f, -1.f,
-            0.f,  0.f, 0.f,  0.f,
-            0.f, -1.f, 0.f,  1.f
-        };
-
-        for (size_t i = 0; i < 4 * 4; i++) {
-            fm[i] = __floats2half2_rn(ff[i*2], ff[i*2+1]);
-        }
-
-        cudaCheckReturn(cudaMemcpy(dev_fm, fm, 4 * 4 * sizeof(DATA_TYPE),
-                                   cudaMemcpyHostToDevice));
-    }
 
     cufftHandle plan;
     cufftCheckReturn(cufftCreate(&plan));
@@ -178,7 +114,10 @@ std::vector<float> benchmark(DATA_TYPE *output,
     cudaCheckReturn(cudaEventRecord(start));
 
 //    cufftCheckReturn(cufftXtExec(plan, dev_data, dev_middle, CUFFT_FORWARD));
-    fft<<<DATA_SIZE / 256, 256>>>(dev_middle, dev_fm, dev_workspace);
+    void *args[2];
+    args[0] = dev_middle;
+    args[1] = NULL;
+    cudaLaunchCooperativeKernel((void*)fft, dim3(DATA_SIZE / 1024, 1, 1), dim3(1024, 1, 1), args);
 
     cudaCheckReturn(cudaEventRecord(stop));
     cudaCheckReturn(cudaEventSynchronize(stop));
