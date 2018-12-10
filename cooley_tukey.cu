f0576d5ef8bdb212eac3a597b0c91e6cb3cf6245
Kazuaki Matsumura <matsumura.k.ak@m.titech.ac.jp>
2018-12-10 19:42:21 +0800

    Debugging radix-4 cooley tukey without Tensor Core

--- cooley_tukey.cu
+++ cooley_tukey.cu
@@ -2,32 +2,34 @@
 #include <cooperative_groups.h>
 
 #define CUDART_PI_F 3.141592654f
+#define DATA_SIZE 16
 
 __device__ __forceinline__ DATA_TYPE mul(DATA_TYPE a, DATA_TYPE b)
 {
-    // (a_r b_r - a_i b_i), (a_r b_i + a_i b_r)
-    DATA_TYPE m  = __floats2half2_rn(-1.0, 1.0);
+    // half a_r = __low2half(a);
+    // half a_i = __high2half(a);
+    // half b_r = __low2half(b);
+    // half b_i = __high2half(b);
+    // return __halves2half2(a_r * b_r - a_i * b_i, a_r * b_i + a_i * b_r);
 
+    DATA_TYPE m  = __floats2half2_rn(-1.0, 1.0);
     // (a_r b_r, a_i b_r)
     DATA_TYPE c = __hmul2(a, __low2half2(b));
     // (- a_i b_i, a_r b_i)
     DATA_TYPE s = __hmul2(m, __hmul2(__lowhigh2highlow(a), __high2half2(b)));
-
     return __hadd2(c, s);
 }
 
 __device__ __forceinline__ DATA_TYPE twiddle(DATA_TYPE a, size_t n, size_t block, size_t row)
 {
     // todo: static
-    float f = 2 * CUDART_PI_F * block * row / n;
-    return mul(a, __floats2half2_rn(cosf(f), - sinf(f)));
+    float f = (-2 * CUDART_PI_F * block * row) / (n * 4);
+    return mul(a, __floats2half2_rn(cosf(f), sinf(f)));
 }
 
-__global__ void fft(DATA_TYPE *data)
+__global__ void fft_kernel(DATA_TYPE *data, size_t n)
 {
-    cooperative_groups::grid_group g = cooperative_groups::this_grid();
-
-    __shared__ DATA_TYPE sm[1024];
+    __shared__ DATA_TYPE sm[256];
     size_t id = blockIdx.x * blockDim.x + threadIdx.x;
 
     const float f_real[] = { 
@@ -39,41 +41,47 @@ __global__ void fft(DATA_TYPE *data)
 
     const float f_imag[] = {
         0.f,  0.f, 0.f,  0.f,
-        0.f,  1.f, 0.f, -1.f,
+        0.f, -1.f, 0.f,  1.f,
         0.f,  0.f, 0.f,  0.f,
-        0.f, -1.f, 0.f,  1.f
+        0.f,  1.f, 0.f, -1.f
     };
 
-    size_t idxl4 = threadIdx.x & 3 * 4;
+    size_t gblock = id / n;
+    size_t block  = gblock % 4;
+    size_t offset = (gblock - block) * n;
+    size_t pos    = id - offset;
+    size_t row    = pos / 4;
+    size_t col    = id % 4;
+
+    size_t idxl4 = col * 4;
     DATA_TYPE f_0 = __floats2half2_rn(f_real[idxl4 + 0], f_imag[idxl4 + 0]);
     DATA_TYPE f_1 = __floats2half2_rn(f_real[idxl4 + 1], f_imag[idxl4 + 1]);
     DATA_TYPE f_2 = __floats2half2_rn(f_real[idxl4 + 2], f_imag[idxl4 + 2]);
     DATA_TYPE f_3 = __floats2half2_rn(f_real[idxl4 + 3], f_imag[idxl4 + 3]);
 
-    for (size_t n  = 4; n <= DATA_SIZE / 4; n *= 4) {
-        if (n != 4) {
-            g.sync();
-        }
+    // transpose, multiply by twiddle factor (W^(col, row))
+    DATA_TYPE x = (n >= 4) ? twiddle(data[offset + col * n + row], n, row, col) : data[id];
 
-        size_t gblock = id / n;
-        size_t offset = gblock * n;
-        size_t block = gblock - (gblock / 4) * 4;
-        size_t pos   = id - offset;
-        size_t row   = pos / 4;
-        size_t col   = pos - row * 4;
-
-        // transpose, multiply by twiddle factor (W^(block, row))
-        DATA_TYPE x = twiddle(data[offset + col * n + row], n, block, row);
-
-        sm[threadIdx.x] = x;
-        __syncwarp();
-
-        // gemm
-        data[id] = __hadd2(__hadd2(mul(sm[threadIdx.x - col + 0], f_0),
-                                   mul(sm[threadIdx.x - col + 1], f_1)),
-                           __hadd2(mul(sm[threadIdx.x - col + 2], f_2),
-                                   mul(sm[threadIdx.x - col + 3], f_3)));
+    if (n == 4 && (id >= 8 && id <= 11)) {
+        float2 f = __half22float2(x);
+        printf("id=%d %f %f\n", id, f.x, f.y);
     }
+
+    sm[threadIdx.x] = x;
+    __syncwarp();
+
+    // gemm
+    data[id] = __hadd2(__hadd2(mul(sm[threadIdx.x - col + 0], f_0),
+                               mul(sm[threadIdx.x - col + 1], f_1)),
+                       __hadd2(mul(sm[threadIdx.x - col + 2], f_2),
+                               mul(sm[threadIdx.x - col + 3], f_3)));
+}
+
+void fft(DATA_TYPE *data)
+{
+   for (size_t n = 1; n <= DATA_SIZE / 4; n *= 4) {
+       fft_kernel<<<DATA_SIZE / 16, 16>>>(data, n);
+   }
 }
 
 std::vector<float> benchmark(DATA_TYPE *output,
@@ -114,10 +122,7 @@ std::vector<float> benchmark(DATA_TYPE *output,
     cudaCheckReturn(cudaEventRecord(start));
 
 //    cufftCheckReturn(cufftXtExec(plan, dev_data, dev_middle, CUFFT_FORWARD));
-    void *args[2];
-    args[0] = dev_middle;
-    args[1] = NULL;
-    cudaLaunchCooperativeKernel((void*)fft, dim3(DATA_SIZE / 1024, 1, 1), dim3(1024, 1, 1), args);
+    fft(dev_middle);
 
     cudaCheckReturn(cudaEventRecord(stop));
     cudaCheckReturn(cudaEventSynchronize(stop));
@@ -158,7 +163,7 @@ std::vector<float> benchmark(DATA_TYPE *output,
     */
     cufftCheckReturn(cufftDestroy(plan));
 
-    cudaCheckReturn(cudaMemcpy(output, dev_output, DATA_SIZE * sizeof(DATA_TYPE),
+    cudaCheckReturn(cudaMemcpy(output, dev_middle, DATA_SIZE * sizeof(DATA_TYPE),
                                cudaMemcpyDeviceToHost));
 
     cudaCheckReturn(cudaFreeHost(middle));
