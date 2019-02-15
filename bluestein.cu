909ee54e1f5c33c459abe0f29c2d052a08066e63
MTMR <matsumura.k.ak@m.titech.ac.jp>
2019-02-15 16:22:42 +0900

    Implemented bluestein

--- bluestein.cu
+++ bluestein.cu
@@ -3,9 +3,54 @@
 
 #define PI 3.141592654f
 
-float calc_w(float r, float n)
+__inline__ __host__ __device__ float calc_w(float r, float n)
 {
-  return fmod(- (2 * PI * r) / n, 2 * PI);
+    return (- (2 * PI * r) / n);
+}
+
+__global__ void kernel_setup_wn(int N, DATA_TYPE *wn)
+{
+    size_t r = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (r <= N - 1) {
+        float np = calc_w((1 / (float)2) * r * r, N);
+        wn[r].x = cosf(np);
+        wn[r].y = sinf(np);
+    }
+}
+
+__global__ void kernel0(int N, int M, DATA_TYPE *x, DATA_TYPE *wn, DATA_TYPE *y)
+{
+    size_t l = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (l <= N - 1) {
+        y[l].x = x[l].x * wn[l].x - x[l].y * wn[l].y;
+        y[l].y = x[l].x * wn[l].y + x[l].y * wn[l].x;
+    }
+    else if (l <= M - 1) {
+        y[l].x = 0.f;
+        y[l].y = 0.f;
+    }
+}
+
+__global__ void kernel1(int M, DATA_TYPE *CY, DATA_TYPE *hh, DATA_TYPE *hCZ)
+{
+    size_t r = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (r <= M - 1) {
+       hCZ[r].x = CY[r].x * hh[r].x - CY[r].y * hh[r].y;
+       hCZ[r].y = CY[r].x * hh[r].y + CY[r].y * hh[r].x;
+    }
+}
+
+__global__ void kernel2(int N, int M, DATA_TYPE *CZ, DATA_TYPE *wn, DATA_TYPE *CX)
+{
+    size_t r = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (r <= N - 1) {
+      CX[r].x = CZ[r].x * wn[r].x - CZ[r].y * wn[r].y;
+      CX[r].y = CZ[r].x * wn[r].y + CZ[r].y * wn[r].x;
+    }
 }
 
 float bluestein(cudaEvent_t start, cudaEvent_t stop,
@@ -14,7 +59,7 @@ float bluestein(cudaEvent_t start, cudaEvent_t stop,
     float time;
 
     size_t N = DATA_SIZE;
-    size_t M = pow(2.0, floor(log2((double)(N - 1)) + 1));
+    size_t M = pow(2.0, ceil(log2((double)(N - 1)) + 1));
 
     cufftHandle plan;
     cufftCheckReturn(cufftCreate(&plan));
@@ -28,118 +73,58 @@ float bluestein(cudaEvent_t start, cudaEvent_t stop,
             NULL, 1, 1, CUDA_C_32F,
             1, &ws, CUDA_C_32F));
 
-    cudaCheckReturn(cudaDeviceSynchronize());
-    cudaCheckReturn(cudaEventRecord(start));
+    DATA_TYPE *h, *dev_h, *dev_hh, *dev_wn;
 
-    DATA_TYPE *h, *hh, *x, *y, *CY, *hCZ, *CZ, *CX;
+    cudaCheckReturn(cudaMallocHost(&h,  M * sizeof(DATA_TYPE)));
+    cudaCheckReturn(cudaMalloc(&dev_h,  M * sizeof(DATA_TYPE)));
+    cudaCheckReturn(cudaMalloc(&dev_hh, M * sizeof(DATA_TYPE)));
 
-    cudaCheckReturn(cudaMallocHost(&h,     M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&hh,    M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&x,     N * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&y,     M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&CY,    M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&hCZ,   M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&CZ,    M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMallocHost(&CX,    N * sizeof(DATA_TYPE)));
+    cudaCheckReturn(cudaMalloc(&dev_wn, M * sizeof(DATA_TYPE)));
 
     for (int l = 0; l <= N-1; l++) {
       float p = calc_w((- 1 / (float)2) * (l * l), N);;
       h[l].x = cosf(p);
       h[l].y = sinf(p);
     }
-    for (int l = M - N + 2; l <= M - 1; l++) {
+    for (int l = M - N + 1; l <= M - 1; l++) {
       h[l] = h[M - l];
     }
-    if (M > (2 * N - 2)) {
-      for (int l = N; l <= M - N + 1; l++) {
-        h[l].x = 0.f;
-        h[l].y = 0.f;
-      }
+    for (int l = N; l <= M - N; l++) {
+      h[l].x = 0.f;
+      h[l].y = 0.f;
     }
 
-    for (int r = 0; r <= M - 1; r++) {
-      hh[r].x = 0.f;
-      hh[r].y = 0.f;
-
-      for (int l = 0; l <= M - 1; l++) {
-        float p = calc_w(r * l, M);
-        hh[r].x += h[l].x * cosf(p) - h[l].y * sinf(p);
-        hh[r].y += h[l].x * sinf(p) + h[l].y * cosf(p);
-      }
-    }
-
-    cudaCheckReturn(cudaMemcpy(x, dev_data, N * sizeof(DATA_TYPE),
-                               cudaMemcpyDeviceToHost));
-
-    for (int l = 0; l <= N - 1; l++) {
-      float p = calc_w((1 / (float)2) * l * l, N);
-      y[l].x = x[l].x * cosf(p) - x[l].y * sinf(p);
-      y[l].y = x[l].x * sinf(p) + x[l].y * cosf(p);
-    }
-    for (int l = N; l <= M - 1; l++) {
-      y[l].x = 0.f;
-      y[l].y = 0.f;
-    }
-
-    for (int r = 0; r <= M - 1; r++) {
-      CY[r].x = 0.f;
-      CY[r].y = 0.f;
-
-      for (int l = 0; l <= M - 1; l++) {
-        float p = calc_w(2 * PI * r * l, M);
-        CY[r].x += y[l].x * cosf(p) - y[l].y * sinf(p);
-        CY[r].y += y[l].x * sinf(p) + y[l].y * cosf(p);
-      }
-    }
-
-    for (int r = 0; r <= M - 1; r++) {
-      hCZ[r].x = CY[r].x * hh[r].x - CY[r].y * hh[r].y;
-      hCZ[r].y = CY[r].x * hh[r].y + CY[r].y * hh[r].x;
-    }
-
-    for (int r = 0; r <= M - 1; r++) {
-      CZ[r].x = 0.f;
-      CZ[r].y = 0.f;
-
-      for (int l = 0; l <= M - 1; l++) {
-        float p = calc_w(- r * l, M);
-        CZ[r].x += hCZ[l].x * cosf(p) - hCZ[l].y * sinf(p);
-        CZ[r].y += hCZ[l].x * sinf(p) + hCZ[l].y * cosf(p);
-      }
-
-      CZ[r].x /= M;
-      CZ[r].y /= M;
-    }
-
-    // for (int r = 0; r <= N - 1; r++) {
-    //   CZ[r].x = 0.f;
-    //   CZ[r].y = 0.f;
-
-    //   for (int l = 0; l <= N - 1; l++) {
-    //     int m = abs(r - l);
-    //     CZ[r].x += y[l].x * h[m].x - y[l].y * h[m].y;
-    //     CZ[r].y += y[l].x * h[m].y + y[l].y * h[m].x;
-    //   }
-
-    //   // CZ[r].x /= M;
-    //   // CZ[r].y /= M;
-    // }
-
-    for (int r = 0; r <= N - 1; r++) {
-      float p = calc_w((1 / (float)2) * r * r, N);
-      CX[r].x = CZ[r].x * cosf(p) - CZ[r].y * sinf(p);
-      CX[r].y = CZ[r].x * sinf(p) + CZ[r].y * cosf(p);
-    }
-
-    cudaCheckReturn(cudaMemcpy(dev_middle, CX, N * sizeof(DATA_TYPE),
+    cudaCheckReturn(cudaMemcpy(dev_h, h, M * sizeof(DATA_TYPE),
                                cudaMemcpyHostToDevice));
 
-    // cufftCheckReturn(cufftXtExec(plan, dev_data, dev_middle, CUFFT_FORWARD));
+    cufftCheckReturn(cufftXtExec(plan, dev_h, dev_hh, CUFFT_FORWARD));
+
+    const size_t tbsize = 128;
+
+    kernel_setup_wn<<<((N + tbsize - 1) / tbsize), tbsize>>>(N, dev_wn);
+
+    cudaCheckReturn(cudaDeviceSynchronize());
+    cudaCheckReturn(cudaEventRecord(start));
+
+    kernel0<<<((M + tbsize - 1) / tbsize), tbsize>>>(N, M, dev_data, dev_wn, dev_middle);
+
+    cufftCheckReturn(cufftXtExec(plan, dev_middle, dev_data, CUFFT_FORWARD));
+
+    kernel1<<<((M + tbsize - 1) / tbsize), tbsize>>>(M, dev_data, dev_hh, dev_middle);
+
+    cufftCheckReturn(cufftXtExec(plan, dev_middle, dev_data, CUFFT_INVERSE));
+
+    kernel2<<<((N + tbsize - 1) / tbsize), tbsize>>>(N, M, dev_data, dev_wn, dev_middle);
 
     cudaCheckReturn(cudaEventRecord(stop));
     cudaCheckReturn(cudaEventSynchronize(stop));
 
     cudaCheckReturn(cudaEventElapsedTime(&time, start, stop));
+
+    cudaCheckReturn(cudaFreeHost(h));
+    cudaCheckReturn(cudaFree(dev_h));
+    cudaCheckReturn(cudaFree(dev_hh));
+    cudaCheckReturn(cudaFree(dev_wn));
 
     return time;
 }
@@ -152,7 +137,7 @@ std::vector<float> benchmark(DATA_TYPE *output,
     std::vector<float> time(2);
     
     size_t N = DATA_SIZE;
-    size_t M = pow(2.0, floor(log2((double)(N - 1)) + 1));
+    size_t M = pow(2.0, ceil(log2((double)(N - 1)) + 1));
 
     /*
       Setup
@@ -161,7 +146,7 @@ std::vector<float> benchmark(DATA_TYPE *output,
 
     cudaCheckReturn(cudaMalloc(&dev_data,   M * sizeof(DATA_TYPE)));
     cudaCheckReturn(cudaMalloc(&dev_middle, M * sizeof(DATA_TYPE)));
-    cudaCheckReturn(cudaMalloc(&dev_output, N * sizeof(DATA_TYPE)));
+    cudaCheckReturn(cudaMalloc(&dev_output, M * sizeof(DATA_TYPE)));
 
     cudaCheckReturn(cudaMemcpy(dev_data, data, N * sizeof(DATA_TYPE),
                                cudaMemcpyHostToDevice));
@@ -183,17 +168,6 @@ std::vector<float> benchmark(DATA_TYPE *output,
     */
     time[0] = bluestein(start, stop, dev_data, dev_middle);
     cudaCheckKernel();
-    //     cudaCheckReturn(cudaDeviceSynchronize());
-    // cudaCheckReturn(cudaEventRecord(start));
-
-    // cufftCheckReturn(cufftXtExec(plan, dev_data, dev_middle, CUFFT_FORWARD));
-
-    // cudaCheckReturn(cudaEventRecord(stop));
-    // cudaCheckReturn(cudaEventSynchronize(stop));
-    // cudaCheckKernel();
-
-    // cudaCheckReturn(cudaEventElapsedTime(&time[0], start, stop));
-
 
     /*
       Scaling
@@ -203,8 +177,8 @@ std::vector<float> benchmark(DATA_TYPE *output,
 
     for (size_t i = 0; i < DATA_SIZE; i++) {
         float2 m = middle[i];
-        m.x /= DATA_SIZE;
-        m.y /= DATA_SIZE;
+        m.x /= N * M;
+        m.y /= N * M;
         middle[i] = m;
     }    
 
@@ -230,7 +204,7 @@ std::vector<float> benchmark(DATA_TYPE *output,
     */
     cufftCheckReturn(cufftDestroy(plan));
 
-    cudaCheckReturn(cudaMemcpy(output, dev_middle, N * sizeof(DATA_TYPE),
+    cudaCheckReturn(cudaMemcpy(output, dev_output, N * sizeof(DATA_TYPE),
                                cudaMemcpyDeviceToHost));
 
     cudaCheckReturn(cudaFreeHost(middle));
